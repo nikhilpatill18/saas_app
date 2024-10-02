8e8c800ac8f8bca2ed2efce0091dbc8ce32c7add
nikhilpatill18 <nikhilpatil6060@gmail.com>
2024-10-02 20:59:52 +0530

    final change

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Element } from 'react-scroll'
-import { features, details } from '../../../constants'
+import { features, details } from '../constants'
 import Button from '../components/Button'
 const Features = () => {
     return (
