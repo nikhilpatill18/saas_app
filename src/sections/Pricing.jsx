b2019ddc007f1dcd106330f4c4670482b51c4fd7
nikhilpatill18 <nikhilpatil6060@gmail.com>
2024-10-02 23:52:04 +0530

    pricing done

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Element } from 'react-scroll'
 import clsx from 'clsx'
+import { plans } from '../constants'
+import CountUp from 'react-countup'
+import Button from '../components/Button'
 
 const Pricing = () => {
     const [Monthly, setMonthly] = useState(false)
@@ -29,6 +32,56 @@ const Pricing = () => {
 
                     </div>
                     {/* pricing section */}
+                    <div className='scroll-hide relative z-2 -mt-12 flex items-start max-xl:g-5 max-xl:overflow-auto max-xl:pt-6'>
+                        {
+                            plans.map((plan, index) => (
+                                <div key={plan.id} className='pricing-plan_first pricing-plan_last  pricing-plan_odd pricing-plan_even relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]'>
+                                    {
+                                        index === 1 && (
+                                            <div className='g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl' />
+                                        )
+                                    }
+                                    <div className={clsx('absolute left-0 right-0 z-2 flex items-center justify-center ', index === 1 ? '-top-6' : '-top-6 xl:-top-11')}>
+                                        <img src={plan.logo} alt="plan" className={clsx('object-contain drop-shadow-2xl', index === 1 ? 'size-[120px]' : 'size-[88px]')} />
+                                    </div>
+                                    <div className={clsx('relative flex flex-col items-center', index === 1 ? 'pt-24' : 'pt-12')}>
+                                        <div className={clsx('small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase', index === 1 ? 'border-p3 text-p3' : 'border-p1 text-p1 ')}>
+                                            {plan.title}
+
+                                        </div>
+                                        <div className='relative z-2 flex items-center justify-center'>
+                                            <div className={clsx('h-num flex items-start', index === 1 ? 'text-p3' : 'text-p4')}>
+                                                $ <CountUp start={plan.priceMonthly} end={Monthly ? plan.priceMonthly : plan.priceYearly} duration={1} useEasing={false} preserveValue />
+                                            </div>
+                                            <div className='small-1 relative top-3 ml-1 uppercase'>
+                                                / mo
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div className={clsx('body-1 relative z-2 mb-10 w-full border-b-s2 pb-9 text-center text-p4', index === 1 && 'border-b')}>
+                                        {plan.caption}
+                                    </div>
+                                    <ul className='mx-auto space-y-4 xl:px-7'>
+                                        {
+                                            plan.features.map((feature) => (
+                                                <li className='relative flex  items-center gap-5'>
+                                                    <img src={"/images/check.png"} alt="" className='size-10 object-contain' />
+                                                    <p className='flex-1'>{feature}</p>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                    <div className='mt-10 flex w-full justify-center'>
+                                        <Button icon={plan.icon} >Get Started</Button>
+                                    </div>
+
+                                </div>
+
+                            ))
+                        }
+                    </div>
                 </div>
             </Element>
         </section>
